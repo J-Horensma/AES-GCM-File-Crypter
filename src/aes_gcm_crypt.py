72412403b09ed5397517ea14f20909c16b5c2e48
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2026-09-14 02:26:41 -0400

    Update fmt.Println output to 'Goodbye World'

--- src/aes_gcm_crypt.py
+++ src/aes_gcm_crypt.py
@@ -22,7 +22,7 @@ __version__ = '1.0.0.0'
 __author__ = 'Joel Horensma'
 __email__ = 'N/A'
 __license__ = 'Commercial-Use License, With Redistribution Restrictions'
-__description__ = 'An AES-GCM cryptography library, for cryptography of variables, files, and folders (Cross-platform).'
+__description__ = 'An AES-GCM cryptography library, for cryptography of variables, files, and/or folders.'
 
 from io import IOBase
 from os import walk, access, R_OK, W_OK, X_OK, replace, remove, fsync
