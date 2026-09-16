4a78f9dea197832ad26d8875a65c616be12a5f14
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2026-09-16 18:31:04 -0400

    Add version argument and refactor main function
    Refactor main entry point to include version argument handling.

--- dist/python_package/aes_gcm_file_crypter/__main__.py
+++ dist/python_package/aes_gcm_file_crypter/__main__.py
@@ -1,4 +1,15 @@
-from .aes_gcm_file_crypter import main
+.aes_gcm_file_crypter import main as gui_main
+
+def main():
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument('--version', action='store_true')
+    ARGUMENTS = PARSER.parse_args()
+
+    if ARGUMENTS.version:
+        print(__version__)
+        return
+
+    gui_main()
 
 if __name__ == '__main__':
     main()
