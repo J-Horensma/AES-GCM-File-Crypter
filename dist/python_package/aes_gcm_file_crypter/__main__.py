0410bdd629371dde1a8142bb23924dff776995e2
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2026-09-19 07:50:09 -0400

    Last error check required

--- dist/python_package/aes_gcm_file_crypter/__main__.py
+++ dist/python_package/aes_gcm_file_crypter/__main__.py
@@ -16,31 +16,31 @@ def cli():
         help='Encrypt a folder',
         epilog=r'Example: python -m aes_gcm_file_crypter encrypt_folder \path\to\folder 256 Password'
     )
-    ENCRYPT_FOLDER.add_argument('folder_path', help='The folder path, to encrypt')
+    ENCRYPT_FOLDER.add_argument('folder_path', help='The folder path to encrypt')
     ENCRYPT_FOLDER.add_argument('key_size', type=int, help='Must be: 128 (Least drive-space used), 192, or 256 (Most secure)')
-    ENCRYPT_FOLDER.add_argument('password', help='A user-chosen password, used for encryption')
+    ENCRYPT_FOLDER.add_argument('password', help='A user-chosen password used for encryption')
     DECRYPT_FOLDER = SUB_PARSER.add_parser(
         'decrypt_folder', 
         help='Decrypt a folder',
         epilog=r'Example: python -m aes_gcm_file_crypter decrypt_folder \path\to\folder Password'
     )
-    DECRYPT_FOLDER.add_argument('folder_path', help='The folder path, to decrypt')
-    DECRYPT_FOLDER.add_argument('password', help='The same password, used to encrypt the folder')
+    DECRYPT_FOLDER.add_argument('folder_path', help='The folder path to decrypt')
+    DECRYPT_FOLDER.add_argument('password', help='The same password used to encrypt the folder')
     ENCRYPT_FILE = SUB_PARSER.add_parser(
         'encrypt_file', 
         help='Encrypt a file',
         epilog=r'Example: python -m aes_gcm_file_crypter encrypt_file \path\to\file.ext 256 Password'
     )
-    ENCRYPT_FILE.add_argument('file_path', help='The file path, to encrypt')
+    ENCRYPT_FILE.add_argument('file_path', help='The file path to encrypt')
     ENCRYPT_FILE.add_argument('key_size', type=int, help='Must be: 128 (Least drive-space used), 192, or 256 (Most secure)')
-    ENCRYPT_FILE.add_argument('password', help='A user-chosen password, used for encryption')
+    ENCRYPT_FILE.add_argument('password', help='A user-chosen password used for encryption')
     DECRYPT_FILE = SUB_PARSER.add_parser(
         'decrypt_file',
         help='Decrypt a file',
         epilog=r'Example: python -m aes_gcm_file_crypter decrypt_file \path\to\file.ext Password'
     )
-    DECRYPT_FILE.add_argument('file_path', help='The file path, to decrypt')
-    DECRYPT_FILE.add_argument('password', help='The same password, used to encrypt the file')
+    DECRYPT_FILE.add_argument('file_path', help='The file path to decrypt')
+    DECRYPT_FILE.add_argument('password', help='The same password used to encrypt the file')
     ARGUMENTS = PARSER.parse_args()
     if ARGUMENTS.COMMAND == 'encrypt_folder':
         from .src.aes_gcm_crypt import aes_gcm_encrypt_folder
