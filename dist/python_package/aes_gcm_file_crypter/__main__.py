5be85e33d6b043f83147eb0493dfe070bbfc95de
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2026-09-19 20:19:17 -0400

    COMPLETED!

--- dist/python_package/aes_gcm_file_crypter/__main__.py
+++ dist/python_package/aes_gcm_file_crypter/__main__.py
@@ -14,7 +14,7 @@ def cli():
     ENCRYPT_FOLDER = SUB_PARSER.add_parser(
         'encrypt_folder', 
         help='Encrypt a folder',
-        epilog=r'Example: python -m aes_gcm_file_crypter encrypt_folder \path\to\folder 256 Password'
+        epilog=r'Example: python -m aes_gcm_file_crypter encrypt_folder /path/to/folder 256 Password'
     )
     ENCRYPT_FOLDER.add_argument('folder_path', help='The folder path to encrypt')
     ENCRYPT_FOLDER.add_argument('key_size', type=int, help='Must be: 128 (Least drive-space used), 192, or 256 (Most secure)')
@@ -22,14 +22,14 @@ def cli():
     DECRYPT_FOLDER = SUB_PARSER.add_parser(
         'decrypt_folder', 
         help='Decrypt a folder',
-        epilog=r'Example: python -m aes_gcm_file_crypter decrypt_folder \path\to\folder Password'
+        epilog=r'Example: python -m aes_gcm_file_crypter decrypt_folder /path/to/folder Password'
     )
     DECRYPT_FOLDER.add_argument('folder_path', help='The folder path to decrypt')
     DECRYPT_FOLDER.add_argument('password', help='The same password used to encrypt the folder')
     ENCRYPT_FILE = SUB_PARSER.add_parser(
         'encrypt_file', 
         help='Encrypt a file',
-        epilog=r'Example: python -m aes_gcm_file_crypter encrypt_file \path\to\file.ext 256 Password'
+        epilog=r'Example: python -m aes_gcm_file_crypter encrypt_file /path/to/file.ext 256 Password'
     )
     ENCRYPT_FILE.add_argument('file_path', help='The file path to encrypt')
     ENCRYPT_FILE.add_argument('key_size', type=int, help='Must be: 128 (Least drive-space used), 192, or 256 (Most secure)')
@@ -37,7 +37,7 @@ def cli():
     DECRYPT_FILE = SUB_PARSER.add_parser(
         'decrypt_file',
         help='Decrypt a file',
-        epilog=r'Example: python -m aes_gcm_file_crypter decrypt_file \path\to\file.ext Password'
+        epilog=r'Example: python -m aes_gcm_file_crypter decrypt_file /path/to/file.ext Password'
     )
     DECRYPT_FILE.add_argument('file_path', help='The file path to decrypt')
     DECRYPT_FILE.add_argument('password', help='The same password used to encrypt the file')
