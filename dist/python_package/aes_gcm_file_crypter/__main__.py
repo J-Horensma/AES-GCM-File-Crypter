1f011f02ace6a60c6cb42e47252a794afd8da4f2
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2026-09-21 06:31:42 -0400

    ERROR, SPELLING, AND GRAMMAR CHECKED

--- dist/python_package/aes_gcm_file_crypter/__main__.py
+++ dist/python_package/aes_gcm_file_crypter/__main__.py
@@ -17,35 +17,35 @@ def cli():
         help='Encrypt a folder',
         epilog=f'Example: python -m aes_gcm_file_crypter encrypt_folder {abspath('/path/to/folder')} 256 Password'
     )
-    ENCRYPT_FOLDER.add_argument('folder_path', help='The folder path to encrypt')
-    ENCRYPT_FOLDER.add_argument('key_size', help='An integer representing the bit size of the random key, created with the user-chosen password, the options are 128 (Least drive-space used), 192, or 256 (Most secure)', type=int)
-    ENCRYPT_FOLDER.add_argument('password', help='A user-chosen password used for encryption')
-    ENCRYPT_FOLDER.add_argument('block_size', help='An optional block size integer representing the amount of bytes for each file chunk to be read (Can cause errors if used incorrectly)', type=int, nargs='?', default=65536)
+    ENCRYPT_FOLDER.add_argument('folder_path', help='The folder path to encrypt.')
+    ENCRYPT_FOLDER.add_argument('key_size', help='An integer representing the bit size of the random key, created with the user-chosen password, the options are 128 (Least drive-space used), 192, or 256 (Most secure).', type=int)
+    ENCRYPT_FOLDER.add_argument('password', help='A user-chosen password for encryption.')
+    ENCRYPT_FOLDER.add_argument('block_size', help='An optional block size integer representing the amount of bytes for each file chunk to be read (Higher is faster, but requires more RAM). The default is 65536 (Can cause errors if used incorrectly).', type=int, nargs='?', default=65536)
     DECRYPT_FOLDER = SUB_PARSER.add_parser(
         'decrypt_folder', 
         help='Decrypt a folder',
         epilog=f'Example: python -m aes_gcm_file_crypter decrypt_folder {abspath('/path/to/folder')} Password'
     )
-    DECRYPT_FOLDER.add_argument('folder_path', help='The folder path to decrypt')
-    DECRYPT_FOLDER.add_argument('password', help='The same password used to encrypt the folder')
-    DECRYPT_FOLDER.add_argument('block_size', help='An optional block size integer representing the amount of bytes for each file chunk to be read (Can cause errors if used incorrectly)', type=int, nargs='?', default=65536)
+    DECRYPT_FOLDER.add_argument('folder_path', help='The folder path to decrypt.')
+    DECRYPT_FOLDER.add_argument('password', help='The same password used to encrypt the folder.')
+    DECRYPT_FOLDER.add_argument('block_size', help='An optional block size integer representing the amount of bytes for each file chunk to be read (Higher is faster, but requires more RAM). The default is 65536 (Can cause errors if used incorrectly).', type=int, nargs='?', default=65536)
     ENCRYPT_FILE = SUB_PARSER.add_parser(
         'encrypt_file', 
         help='Encrypt a file',
         epilog=f'Example: python -m aes_gcm_file_crypter encrypt_file {abspath('/path/to/file.ext')} 256 Password'
     )
-    ENCRYPT_FILE.add_argument('file_path', help='The file path to encrypt')
-    ENCRYPT_FILE.add_argument('key_size', help='An integer representing the bit size of the random key, created with the user-chosen password, the options are 128 (Least drive-space used), 192, or 256 (Most secure)', type=int)
-    ENCRYPT_FILE.add_argument('password', help='A user-chosen password used for encryption')
-    ENCRYPT_FILE.add_argument('block_size', help='An optional block size integer representing the amount of bytes for each file chunk to be read (Can cause errors if used incorrectly)', type=int, nargs='?', default=65536)
+    ENCRYPT_FILE.add_argument('file_path', help='The file path to encrypt.')
+    ENCRYPT_FILE.add_argument('key_size', help='An integer representing the bit size of the random key, created with the user-chosen password, the options are 128 (Least drive-space used), 192, or 256 (Most secure).', type=int)
+    ENCRYPT_FILE.add_argument('password', help='A user-chosen password for encryption.')
+    ENCRYPT_FILE.add_argument('block_size', help='An optional block size integer representing the amount of bytes for each file chunk to be read (Higher is faster, but requires more RAM). The default is 65536 (Can cause errors if used incorrectly).', type=int, nargs='?', default=65536)
     DECRYPT_FILE = SUB_PARSER.add_parser(
         'decrypt_file',
         help='Decrypt a file',
         epilog=f'Example: python -m aes_gcm_file_crypter decrypt_file {abspath('/path/to/file.ext')} Password'
     )
-    DECRYPT_FILE.add_argument('file_path', help='The file path to decrypt')
-    DECRYPT_FILE.add_argument('password', help='The same password used to encrypt the file')
-    DECRYPT_FILE.add_argument('block_size', help='An optional block size integer representing the amount of bytes for each file chunk to be read (Can cause errors if used incorrectly)', type=int, nargs='?', default=65536)
+    DECRYPT_FILE.add_argument('file_path', help='The file path to decrypt.')
+    DECRYPT_FILE.add_argument('password', help='The same password used to encrypt the file.')
+    DECRYPT_FILE.add_argument('block_size', help='An optional block size integer representing the amount of bytes for each file chunk to be read (Higher is faster, but requires more RAM). The default is 65536 (Can cause errors if used incorrectly).', type=int, nargs='?', default=65536)
     ARGUMENTS = PARSER.parse_args()
     if ARGUMENTS.COMMAND == 'encrypt_folder':
         from .src.aes_gcm_crypt import aes_gcm_encrypt_folder
