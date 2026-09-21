2a63af43565b7f159a6021a204dcaf96458003d7
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2026-09-21 05:18:47 -0400

    ERROR, SPELLING, AND GRAMMAR CHECKED

--- dist/python_package/aes_gcm_file_crypter/src/aes_gcm_crypt.py
+++ dist/python_package/aes_gcm_file_crypter/src/aes_gcm_crypt.py
@@ -398,7 +398,7 @@ def aes_gcm_encrypt_file(FILE_PATH, KEY_SIZE, PASSWORD, BLOCK_SIZE=None):
     except BaseException as ERROR:
         if isfile(FILE_PATH + '.tmp'):
             remove(FILE_PATH + '.tmp')
-        return [False, f'ERROR!:\n{ERROR if str(ERROR).strip() else 'An unknown error occurred!'}\nFile path: {FILE_PATH}']
+        return [False, f'ERROR!:\n{ERROR if str(ERROR).strip() else 'An unknown error occurred! ' + 'Try a smaller block size.' if BLOCK_SIZE > 65536 else ''}\nFile path: {FILE_PATH}']
 
 #THIS FUNCTION:
 #1.) REQUIRES FILE PATH AND PASSWORD STRINGS
@@ -499,7 +499,7 @@ def aes_gcm_decrypt_file(FILE_PATH, PASSWORD, BLOCK_SIZE=None):
     except BaseException as ERROR:
         if isfile(FILE_PATH + '.tmp'):
             remove(FILE_PATH + '.tmp')
-        return [False, f'ERROR!:\n{ERROR if str(ERROR).strip() else 'An unknown error occurred!'}\nFile path: {FILE_PATH}']
+        return [False, f'ERROR!:\n{ERROR if str(ERROR).strip() else 'An unknown error occurred! ' + 'Try a smaller block size.' if BLOCK_SIZE > 65536 else ''}\nFile path: {FILE_PATH}']
 
 #THIS FUNCTION:
 #1.) REQUIRES A PLAINTEXT STRING OR BYTES TYPE VARIABLE, KEY SIZE INTEGER, AND A PASSWORD STRING
